e7631ee533cd4fffb54a0ca6afaf29d595c5031c
Henk Mollema <henkmollema@gmail.com>
2015-10-29 21:29:30 +0100

    Add tests

---
+++ test/CryptoHelper.Tests/CryptoHelperTests.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+
+namespace CryptoHelper.Tests
+{
+    public class CryptoHelperTests
+    {
+        private const string Password = "VerySecuryPassword";
+
+        [Fact]
+        public void HashPassword_Returns_HashedPassword()
+        {
+            var hashed = Crypto.HashPassword(Password);
+            Assert.NotEmpty(hashed);
+        }
+
+        [Fact]
+        public void VeryifyHashedPasswordWithCorrectPassword_Returns_CorrectResult()
+        {
+            var hashed = Crypto.HashPassword(Password);
+            var result = Crypto.VerifyHashedPassword(hashed, Password);
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void VeryifyHashedPasswordWithIncorrectPassword_Returns_CorrectResult()
+        {
+            var hashed = Crypto.HashPassword(Password);
+            var result = Crypto.VerifyHashedPassword(hashed, "WrongPassword");
+            Assert.False(result);
+        }
+    }
+}
