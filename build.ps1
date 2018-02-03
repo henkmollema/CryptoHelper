985b902d2b1457cc2f57d314aacd5bd5a241a99b
Henk Mollema <henkmollema@gmail.com>
2018-02-03 12:36:35 +0100

    Fix build PowerShell script

--- build.ps1
+++ build.ps1
@@ -1,2 +1,13 @@
+Write-Host "Executing build..."
 dotnet build
+if ($LASTEXITCODE -ne 0) {
+    Write-Error  "Executing build failed"
+    exit $LASTEXITCODE
+}
+
+Write-Host "Executing tests..."
 dotnet test .\test\CryptoHelper.Tests\CryptoHelper.Tests.csproj
+if ($LASTEXITCODE -ne 0) {
+    Write-Error  "Executing tests failed"
+    exit $LASTEXITCODE
+}
